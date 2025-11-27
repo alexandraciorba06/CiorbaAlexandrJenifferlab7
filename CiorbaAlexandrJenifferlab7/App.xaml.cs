04329ecbc5d2269e6d6a98d687743727a4ea337e
alexandraciorba06 <alexandra.jeniffer.ciorba@stud.ubbcluj.ro>
2025-11-27 15:36:38 +0200

    lab8

--- CiorbaAlexandrJenifferlab7/App.xaml.cs
+++ CiorbaAlexandrJenifferlab7/App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace CiorbaAlexandrJenifferlab7
+﻿using System;
+using CiorbaAlexandrJenifferlab7.Data;
+using System.IO;
+
+namespace CiorbaAlexandrJenifferlab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
---
+++ CiorbaAlexandrJenifferlab7/Models/ShopList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiorbaAlexandrJenifferlab7.Models
+{
+    using SQLite;
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
