71791ce0f7cfd7eb3280e6025dff88e8acd3c6ab
alexandraciorba06 <alexandra.jeniffer.ciorba@stud.ubbcluj.ro>
2025-12-14 20:52:21 +0200

    modificari

--- CiorbaAlexandrJenifferlab7/ProductPage.xaml.cs
+++ CiorbaAlexandrJenifferlab7/ProductPage.xaml.cs
@@ -8,6 +8,18 @@ public partial class ProductPage : ContentPage
         InitializeComponent();
         sl = slist;
     }
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.SaveProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var product = listView.SelectedItem as Product;
+        await App.Database.DeleteProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
     async void OnAddButtonClicked(object sender, EventArgs e)
     {
 
@@ -25,19 +37,6 @@ public partial class ProductPage : ContentPage
 
             await Navigation.PopAsync();
         }
-    }
-        async void OnSaveButtonClicked(object sender, EventArgs e)
-    {
-        var product = (Product)BindingContext;
-        await App.Database.SaveProductAsync(product);
-        listView.ItemsSource = await App.Database.GetProductsAsync();
-    }
-
-    async void OnDeleteButtonClicked(object sender, EventArgs e)
-    {
-        var product = listView.SelectedItem as Product;
-        await App.Database.DeleteProductAsync(product);
-        listView.ItemsSource = await App.Database.GetProductsAsync();
     }
 
     protected override async void OnAppearing()
